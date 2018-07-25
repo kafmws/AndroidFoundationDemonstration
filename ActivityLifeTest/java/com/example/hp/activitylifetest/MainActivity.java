001b7426a94d57bac0dd74b04c4fb978af8d542e
kafmws <740721665@qq.com>
2018-07-25 10:24:25 +0800

    纠正错误

--- ActivityLifeTest/java/com/example/hp/activitylifetest/MainActivity.java
+++ ActivityLifeTest/java/com/example/hp/activitylifetest/MainActivity.java
@@ -1,12 +1,8 @@
 package com.example.hp.activitylifetest;
 
-import android.app.Dialog;
 import android.content.Intent;
-import android.nfc.Tag;
-import android.os.PersistableBundle;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -18,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(log,"on Create");
         setContentView(R.layout.main_layout);
 
         Button button1 = findViewById(R.id.Button_1);
@@ -40,15 +37,10 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
-        Log.d(log,"on Create");
-    }
-
-    @Override
     protected void onStart() {
         super.onStart();
         Log.d(log,"on Start");
+        finish();
     }
 
     @Override
